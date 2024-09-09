d18d3169285f35183310b41f0ec747f6eddbb2c4
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-09 16:42:00 +0200

    added buttons

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-//one var for first number
-//one for the second one
-//var for the operator(ie, +,-,*) etc
+let firstNumber = 0;
+let secondNumber = 0;
+//create function called operate to call button and use one of the functions
+//use eventListeners on the buttons for operate, to call one of them
+//when you click button, populate display
+
+function operate() {
+    const display = document.querySelector("#display");
+}
+
+//use mousedown instead of click?
+//create functions to populate the "display"/input field
+
+//functions for various mathematical operations.
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
