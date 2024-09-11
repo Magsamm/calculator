add856293349fea7c63987c0ed4c50696e51c423
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-11 16:32:56 +0200

    calculator now displays numbers when pressing buttons

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let firstNumber = 0;
 let secondNumber = 0;
-const container = document.querySelector(".container");
-
 let userInput = "";
-const input = document.querySelector("#input");
+const container = document.querySelector(".container");
 
 document.querySelectorAll("button").forEach((button) => {
     button.addEventListener("click", btnAction);
 });
-
 function btnAction(event) {
+    const input = document.querySelector("input");
     const btn = event.target;
-    console.info("button:", btn.textContent, btn);
+    input.value = "";
+    (userInput += btn.textContent), btn;
+    input.value += userInput;
 }
 
 function add(numOne, numTwo) {
