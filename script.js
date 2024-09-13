0fbee5db7deab084b21ffa2df62710426a71535c
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-13 17:12:45 +0200

    added rudimantary logic for the plus operator. need to use split to split the numbers instead tho, so i can do math on them

--- script.js
+++ script.js
@@ -1,61 +1,68 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let userInput = "";
+//empty arr to store the two numbers
+//try adding numbers to the arr instead of two variables
+const arr = [];
 
 const container = document.querySelector(".container");
-
-//query buttons, and the input field and add eventListeners
-//add the value of the button pressed to the input, so it acts as a display
-
-//attach eventListeners to buttons. query input field and add button value to input
-// add second number to second variable
-
-//key event for = - + etc? check docs
-// when equals is pressed. store first number in firstNumber, call clear display, make ready for //secondNumber
-
-//once equals has been clicked, remove eventListeners as per //eloquentjavascript?
-//https://eloquentjavascript.net/15_event.html#c-nMrNUG0bzK
 
 //populate display
 const input = document.querySelector("input");
 for (const element of document.querySelectorAll(".number")) {
     element.addEventListener("mousedown", () => {
         input.value += element.textContent;
-        //add input to global var
-        userInput = input.value;
-        //add userInput to firstNumber
-        firstNumber = userInput;
+        firstNumber = input.value;
         console.log(firstNumber);
-        //empty userInput afterwards
-        userInput = "";
+
+        //add input to global var
+        //add userInput to firstNumber
+        //https://www.w3schools.com/jsref/jsref_push.asp#:~:text=The%20push()%20method%20adds,method%20returns%20the%20new%20length.
+        //push variables to array
+
+        //split string userInput, then add them to var one and two?
     });
 }
+
+// if (
+//     document.querySelector(".operator", "#plus").addEventListener("mousedown", () => {
+//         //clear input, add second number to secondNumber
+//     })
+// );
+
+//make it work for plus, then see if you cant make it work for all
+const operators = document.querySelector(".operator");
+for (const element of document.querySelectorAll("#plus")) {
+    element.addEventListener("mousedown", () => {
+        //slice out the +?
+        input.value += element.textContent;
+        secondNumber = input.value;
+    });
+}
+
 //clear display button
-const clearDisplay = document.querySelector("#clear");
-clearDisplay.addEventListener("mousedown", () => {
+document.querySelector("#clear").addEventListener("mousedown", () => {
     input.value = "";
 });
 
 //ADD backspace button in addition to clear all?
-
-//when user presses +-*/, call add/subtract/divide/multiply
-// store the first number, prepare for the 2nd number
-//display results
-// make the function call the appropriate function on the numbers
-//attach event listeners to this one? one for each button, that calls the appropriate function?
 //check out array methods? maybe need to use slice, splice etc
 //go back to objects lesson?
 
 //use an arr to store the two numbers, and map?
+//if operator pressed, clear input and wait for second number
+//add userInput to secondNumber
+//add class and ID to operators
 
-function operate(numOne, numTwo) {
-    //if operator button pressed/aka +- etc, store first number in var
-    //wait for user to enter second number, then call a function on them
-    //empty arr to store the two numbers
-    const arr = [];
-    if (document.querySelector("#plus")) {
-    }
-}
+//store first number
+//when operator button pressed, clear input
+//wait for second number
+//press equals
+//call one of the functions?
+
+//if equals button pressed, call appropriate function on number with operate
+document.querySelector("#equals").addEventListener("mousedown", () => {});
+function operate(numOne, numTwo) {}
 
 function add(numOne, numTwo) {
     return numOne + numTwo;
