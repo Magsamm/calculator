e1da09d345197e2ebd87c5f238d3263caa66c7d6
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-13 14:49:17 +0200

    made populate display function also addd userInput data to firstNumber, then clear userInput afterwards

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let firstNumber = 0;
 let secondNumber = 0;
 let userInput = "";
+
 const container = document.querySelector(".container");
 
 //query buttons, and the input field and add eventListeners
@@ -12,30 +13,49 @@ const container = document.querySelector(".container");
 //key event for = - + etc? check docs
 // when equals is pressed. store first number in firstNumber, call clear display, make ready for //secondNumber
 
-//once equals has been clicked, remove eventListeners as per eloquentjavascript?
+//once equals has been clicked, remove eventListeners as per //eloquentjavascript?
 //https://eloquentjavascript.net/15_event.html#c-nMrNUG0bzK
 
-//clear display button
-
+//populate display
 const input = document.querySelector("input");
 for (const element of document.querySelectorAll(".number")) {
     element.addEventListener("mousedown", () => {
         input.value += element.textContent;
+        //add input to global var
         userInput = input.value;
+        //add userInput to firstNumber
+        firstNumber = userInput;
+        console.log(firstNumber);
+        //empty userInput afterwards
+        userInput = "";
     });
 }
-
+//clear display button
 const clearDisplay = document.querySelector("#clear");
 clearDisplay.addEventListener("mousedown", () => {
     input.value = "";
 });
+
+//ADD backspace button in addition to clear all?
 
 //when user presses +-*/, call add/subtract/divide/multiply
 // store the first number, prepare for the 2nd number
 //display results
 // make the function call the appropriate function on the numbers
 //attach event listeners to this one? one for each button, that calls the appropriate function?
-function operate(numOne, numTwo) {}
+//check out array methods? maybe need to use slice, splice etc
+//go back to objects lesson?
+
+//use an arr to store the two numbers, and map?
+
+function operate(numOne, numTwo) {
+    //if operator button pressed/aka +- etc, store first number in var
+    //wait for user to enter second number, then call a function on them
+    //empty arr to store the two numbers
+    const arr = [];
+    if (document.querySelector("#plus")) {
+    }
+}
 
 function add(numOne, numTwo) {
     return numOne + numTwo;
