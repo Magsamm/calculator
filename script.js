5f17e8bfeedbf56e84d77c1cca35f70917245d0a
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-17 23:07:03 +0200

    numbers now get added correctly to the array

--- script.js
+++ script.js
@@ -14,18 +14,29 @@ for (const element of document.querySelectorAll(".number")) {
     });
 }
 
-//when you click the operator button, add first whole number to firstNumber, wipe input
-//then wait for second input?
-//be input?
+//FUCKING FINALLY
+//just to wait till user pressed an operator :sob:
+//now need to wipe input value
+//wait for second number
+//add to array?
+//call appropriate functions
+//display results
+//add [0] to firstNumber and [1] to second
+
 const operators = document.querySelector(".operator");
-for (const element of document.querySelectorAll(".operator")) {
-    element.addEventListener("mousedown", () => {});
+for (const element of document.querySelectorAll("#plus")) {
+    element.addEventListener("mousedown", () => {
+        arr.push(input.value);
+        input.value = "";
+        console.log(arr);
+    });
 }
 
 //clear display button
 document.querySelector("#clear").addEventListener("mousedown", () => {
     input.value = "";
-    arr = [];
+    //reset array to zero
+    arr.length = 0;
 });
 
 //if equals button pressed, call appropriate function on number with operate
