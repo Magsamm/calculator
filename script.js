3c752f71b579194652441f2db667d6eefdda09b0
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-18 11:18:34 +0200

    use let with array instead of const. easier to make sure its empty. im sure there's a better way TODO

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let userInput = "";
 let firstNumber = "";
 let secondNumber = "";
-const array = [];
+let result = 0;
+let array = [];
+let arrayOfNumbers = [];
 
 //empty arr to store the two numbers
 //try adding numbers to the arr instead of two variables
@@ -10,38 +12,42 @@ const array = [];
 const container = document.querySelector(".container");
 //populate display
 const input = document.querySelector("input");
-
 for (const element of document.querySelectorAll(".number")) {
     element.addEventListener("mousedown", () => {
         input.value += element.textContent;
     });
 }
 
-//add [0] to firstNumber and [1] to second
-//add the result to a results var at the end of each function that gets called when the user presses = ??
-//find out how to use splice to only evaluate 2 numbers at a time
-//push number, when two values in array, call function?
+//query .operator class
+//adds value of input and pushes it to an array when an operator is pressed
 for (let element of document.querySelectorAll(".operator")) {
     element.addEventListener("mousedown", () => {
-        array.push(input.value);
+        //push input.value to array
+        //clear input value to make it ready for the next number
+        array.push(Number(input.value));
         input.value = "";
+        console.log(array);
     });
-
-    //if operator == "+" call function?
+}
+//call function with numOne,NumTwo as args
+//try calling function with values from array as args to see if it works
+function operate(numOne, numTwo) {
+    //take two numbers and call one of the functions on them
 }
 
 //clear display button
 document.querySelector("#clear").addEventListener("mousedown", () => {
+    //reset array and empty input.value
     input.value = "";
-    array.lengths = 0;
-    //reset array to zero
+    array = [];
 });
 
 //if equals button pressed, call appropriate function on number with operate
 document.querySelector("#equals").addEventListener("mousedown", () => {});
-function operate(numOne, numTwo) {}
 
-function add(numOne, numTwo) {
+function add() {
+    let numOne = array[0];
+    let numTwo = array[1];
     return numOne + numTwo;
 }
 
