dbb46ded6f49ce41287f8c9d11770de10d41e050
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-24 14:35:11 +0200

    fixed pressing equals before number resulting in empty value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const array = [];
 let result = 0;
-let operandCounter = 2;
 let saveOperands = "";
+let finalResult = 0;
 const container = document.querySelector(".container");
 //populate display
 const input = document.querySelector("input");
@@ -19,33 +19,33 @@ for (let element of document.querySelectorAll(".operator")) {
             isNaN(input.value) === true ||
             input.value === 0 ||
             input.value === "" ||
-            saveOperands.length > 2
+            saveOperands.length >= 2
         ) {
             input.value = "";
         } else {
             array.push(Number(input.value));
-
             input.value = "";
         }
     });
 }
 //get button.id
-//increase or reset operandCounter
+//add textContent to saveOperands to keep track of operators
+//don't accept more inputs if saveOperands >= 2
 const buttons = document.querySelectorAll(".operator");
 let buttonId = "";
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
         buttonId = button.id;
-        if (button.id === "clear" || saveOperands.length >= 2) {
-            input.value = "";
+        if (button.id === "clear" || saveOperands.length >= 2 || button.id === "equals") {
+            saveOperands = "";
         } else {
             saveOperands += button.textContent;
-            console.log(saveOperands);
         }
     });
 });
-//if clicks > 1, prioritize the next buttonId?
-
+//if saveOperands > 2, and saveOperands[1] === "+"?
+//console.log((finalResult += add()));
+//splice finalResult into the array at position[0]
 function operate(numOne, numTwo) {
     if (buttonId === "plus") {
         input.value = add(numOne, numTwo);
@@ -58,20 +58,11 @@ function operate(numOne, numTwo) {
     }
 }
 
-//focus on making sure you can only do one "operation" at a time.
-// so for example, you can only press
-//of array === 2, operate with the current operator
-//if user enters another operator
-//use result in the next calculation, with the next operator
-//dont allow a new operator until the current calculation is done?
-// For example, when both operands are full, make it evaluate on next operator input, and then just bring the answer and operator over to next set
-
 for (let element of document.querySelectorAll("#equals")) {
     element.addEventListener("mousedown", () => {
         if (array.length > 0) {
-            let final = 0;
             operate(array[0], array[1]);
-            console.log((final += add()));
+            console.log(saveOperands);
         }
     });
 }
@@ -80,6 +71,7 @@ for (let element of document.querySelectorAll("#equals")) {
 document.querySelector("#clear").addEventListener("mousedown", () => {
     //reset array and empty input.value
     input.value = "";
+    saveOperands = "";
     array.length = 0;
 });
 //math functions to perform calculations
@@ -101,7 +93,7 @@ function multiply() {
 function divide() {
     let result = array[0] / array[1];
     if (result === Infinity || isNaN(result) === true) {
-        return (input.value = "cannot divide by 0");
+        return (input.value = "Can't divide by 0!");
     }
     return result.toFixed(2);
 }
