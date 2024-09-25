f4b74324d155e1b16b4166c23510f9765a39cfd3
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-25 19:57:53 +0200

    latest changes using variables instad of array. doesnt work. github is complaining

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let array = [];
 let result = 0;
-let finalResult = 0;
-let operatorClick = false;
+let number = "";
+let numberOne = "";
+let numberTwo = "";
 const container = document.querySelector(".container");
 //populate display
 const input = document.querySelector("input");
@@ -15,89 +15,75 @@ for (const element of document.querySelectorAll(".number")) {
 //check if an operator has been clicked
 //get button.id
 const buttons = document.querySelectorAll(".operator");
-const numbers = document.querySelectorAll(".number");
 let buttonId = "";
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        buttonId = button.id;
-        if (!(array.length < 1 || button.id === "clear" || button.id === "equals")) {
+        if (!(button.id === "clear" || button.id === "equals")) {
+            buttonId = button.id;
         }
     });
 });
-
-//maybe just push finalResult to the array regardless? and wipe it?
-
+//first number
 for (let element of document.querySelectorAll(".operator")) {
     element.addEventListener("mousedown", () => {
-        if (!(isNaN(input.value) === true || input.value === 0 || input.value === "")) {
-            array.push(Number(input.value));
-            input.value = "";
-        }
+        number = input.value;
+        input.value = "";
+        number.split("+/-*", "");
+        numberOne = number[0];
+        numberTwo = number[1];
+        console.log(number);
     });
 }
+
+//if buttonId === "equals", present result in input.value
+//otherwise, evaluate 1 pair at a time
 for (let element of document.querySelectorAll("#equals")) {
     element.addEventListener("mousedown", () => {
-        if (array.length > 1) {
-            operate();
-            array = [];
-            array.splice(2, 2, finalResult);
-            console.log(array);
-        }
+        operate();
+        input.value = result;
     });
 }
 
 //clear display button
-
-document.querySelector("#clear").addEventListener("click", () => {
-    //reset array and empty input.value, saveOperator.
+document.querySelector("#clear").addEventListener("mousedown", () => {
+    //reset array and empty input.value.
     // we use the .forEach method to iterate through each button
-
     input.value = "";
-    saveOperator = "";
-    array.length = 0;
+    number = "";
+    buttonId = "";
 });
-
-function operate(numOne, numTwo) {
+function operate() {
     if (buttonId === "plus") {
-        input.value = add(numOne, numTwo);
+        input.value = add();
     } else if (buttonId === "minus") {
-        input.value = subtract(numOne, numTwo);
+        input.value = subtract();
     } else if (buttonId === "multiply") {
-        input.value = multiply(numOne, numTwo);
+        input.value = multiply();
     } else {
-        input.value = divide(numOne, numTwo);
+        input.value = divide();
     }
 }
 
 //perform calculations with chosen operator.
 function add() {
-    let result = array[0] + array[1];
-    array.push(result);
-    finalResult += result;
+    result = numberOne + numberTwo;
     return result;
 }
 
 function subtract() {
-    let result = array[0] - array[1];
-    finalResult += result;
-
-    array.push(result);
+    result = numOne - numTwo;
     return result;
 }
 
 function multiply() {
-    let result = array[0] * array[1];
-    finalResult += result;
-    array.push(result);
+    result = numOne * numTwo;
     return result;
 }
 //handles 0 division, and rounds number to max 2 decimal places.
 function divide() {
-    let result = array[0] / array[1];
+    let result = numOne / numTwo;
     if (result === Infinity || isNaN(result) === true) {
         return (input.value = "Can't divide by 0!");
     }
-    array.push(result);
-    finalResult += result;
     return Number(result).toFixed(2);
 }
