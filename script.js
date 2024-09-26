10216d9dce22495b87264d3119304c0336d3ee01
Magsamm <19634056+Magsamm@users.noreply.github.com>
2024-09-27 00:36:48 +0200

    logic for calculator done. still need to work on the CSS.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let array = [];
 let result = 0;
-let finalResult = 0;
-let operatorClick = false;
+let buttonId = "";
 const container = document.querySelector(".container");
 //populate display
 const input = document.querySelector("input");
@@ -11,83 +10,73 @@ for (const element of document.querySelectorAll(".number")) {
     });
 }
 
-//need to disable operators until a number button has been clicked again after the first one
-//check if an operator has been clicked
 //get button.id
 const buttons = document.querySelectorAll(".operator");
-const numbers = document.querySelectorAll(".number");
-let buttonId = "";
 buttons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (!(array.length < 1 || button.id === "clear" || button.id === "equals")) {
-            buttonId = button.id;
-        }
+        buttonId = button.id;
     });
 });
 
 for (let element of document.querySelectorAll(".operator")) {
     element.addEventListener("mousedown", () => {
         array.push(Number(input.value));
-        operate();
         input.value = "";
     });
 }
-for (let element of document.querySelectorAll("#equals")) {
-    element.addEventListener("mousedown", () => {
-        if (array.length > 1) {
-            //array.splice(2, 2, finalResult);
-            input.value = Number(array[array.length - 1]);
-            console.log(array);
-        }
-    });
-}
+
+//if user presses equals, present latest result from input.value
+const btn = document.querySelector("#equals");
+btn.addEventListener("mousedown", function () {
+    array.push(Number(input.value));
+    input.value = operate();
+    array = [];
+});
 
 //clear display button
-
 document.querySelector("#clear").addEventListener("click", () => {
-    //reset array and empty input.value, saveOperator.
-    // we use the .forEach method to iterate through each button
-
+    //reset array and empty input.value.
     input.value = "";
     array.length = 0;
+    result = 0;
 });
 
 function operate(numOne, numTwo) {
     if (buttonId === "plus") {
-        input.value = add(numOne, numTwo);
+        return add(numOne, numTwo);
     } else if (buttonId === "minus") {
-        input.value = subtract(numOne, numTwo);
+        return subtract(numOne, numTwo);
     } else if (buttonId === "multiply") {
-        input.value = multiply(numOne, numTwo);
+        return multiply(numOne, numTwo);
     } else {
-        input.value = divide(numOne, numTwo);
+        return divide(numOne, numTwo);
     }
 }
 
-//perform calculations with chosen operator.
 function add() {
-    let result = array[0] + array[1];
-    array.push(result);
-    return result;
+    if (!isNaN(array[0]) && !isNaN(array[1])) {
+        return array[0] + array[1];
+    }
+    return (input.value = "Non integer value!");
 }
 
 function subtract() {
-    let result = array[0] - array[1];
-    array.push(result);
-    return result;
+    if (!isNaN(array[0]) && !isNaN(array[1])) {
+        return array[0] - array[1];
+    }
+    return (input.value = "Non integer value!");
 }
 
 function multiply() {
-    let result = array[0] * array[1];
-    array.push(result);
-    return result;
+    if (!isNaN(array[0]) && !isNaN(array[1])) {
+        return array[0] * array[1];
+    }
+    return (input.value = "Non integer value!");
 }
 //handles 0 division, and rounds number to max 2 decimal places.
 function divide() {
-    let result = array[0] / array[1];
-    if (result === Infinity || isNaN(result) === true) {
+    if (array[0] / array[1] === Infinity || isNaN(array[0] / array[1]) === true) {
         return (input.value = "Can't divide by 0!");
     }
-    array.push(result);
-    return Number(result).toFixed(2);
+    return Number(array[0] / array[1]).toFixed(2);
 }
